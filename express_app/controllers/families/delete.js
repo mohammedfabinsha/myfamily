108eecb47454f0478246c957e4fe14678753f342
Muhammed Rameez K <muhammedrameez1010@gmail.com>
2022-10-21 12:04:47 +0530

    feat:familyControllers

--- express_app/controllers/families/delete.js
+++ express_app/controllers/families/delete.js
@@ -2,7 +2,8 @@ const { Family } = require("../../models")
 
 
 
-module.exports = async function (data) {
-
-
+module.exports = async function (id) {
+    const deletedFamily = await Family.findOne({ where: { id: id } });
+    await deletedFamily.destroy();
+    return deletedFamily;
 }
--- express_app/controllers/families/getSingle.js
+++ express_app/controllers/families/getSingle.js
@@ -2,9 +2,12 @@ const { Family } = require("../../models")
 
 
 
-module.exports = async function (data) {
-
-
-
-
+module.exports = async function (id) {
+    const family = await Family.findOne({ where: { id: id } });
+    return family;
 }
+
+
+
+
+
